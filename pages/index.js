cbbe1bf29aa7d620cb7e3fabdd51e6c41e9a7384
“Akpughe” <davidakpughe2@gmail.com>
2022-08-06 15:18:05 +0200

    add: enter pin

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,47 @@
 import MainLayout from '../components/MainLayout';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Home() {
-  return (
-    <MainLayout>
-      <div>
-        <p className="font-sans text-sm font-normal uppercase">total revenue</p>
-        <div className="mt-4">
-          <h1 className="text-4xl">N 2,230,021.00</h1>
-        </div>
-      </div>
+  const router = useRouter();
+  const [pin, setPin] = useState();
 
-      <div className="max-w-xl mt-10">
-        <div className='mb-4'>
-          <p className="uppercase font-sans text-sm font-normal tracking-wider">recent transactions</p>
+  const enterButton = (e) => {
+    e.preventDefault();
+    pin !== "1234" ? alert('Wrong pin') : router.push('/home');
+  };
+
+  return (
+    <div className="m-auto w-fit my-10">
+      <div className="flex flex-col  bg-white p-6">
+        <div className="">
+          <form className="" onSubmit={(e) => enterButton(e)}>
+            <div className="mt-4">
+              <label
+                className="block font-bold text-[#3A4374] text-sm"
+                htmlFor="title"
+              >
+                Password
+              </label>
+              <input
+                name="password"
+                value={pin}
+                onChange={(e) => setPin(e.target.value)}
+                type="password"
+                placeholder="••••"
+              />
+            </div>
+            <div className="mt-8">
+              <button
+                class="flex items-center justify-center w-full font-bold text-xs text-white rounded-lg px-8 h-11 bg-[#ad20ea]"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-        <table className="w-full text-sm text-left text-black">
-          <thead className="text-xs text-black uppercase">
-            <tr>
-              <th className='py-2'>Ref</th>
-              <th className='py-2'>Amount</th>
-              <th className='py-2'>Date</th>
-            </tr>
-          </thead>
-          <tbody className=''>
-            <tr className="bg-white border-b py-1">
-              <td>RF25464564456</td>
-              <td>100,000</td>
-              <td>6/08/2022</td>
-            </tr>
-            <tr className="bg-white border-b py-1">
-              <td>RF25464564456</td>
-              <td>100,000</td>
-              <td>6/08/2022</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-    </MainLayout>
+    </div>
   );
 }
