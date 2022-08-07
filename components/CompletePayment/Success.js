fe202d2f73005d86b1bdd7ea595fab1e739d86ae
“Akpughe” <davidakpughe2@gmail.com>
2022-08-07 21:41:50 +0200

    fix build error

--- components/CompletePayment/Success.js
+++ components/CompletePayment/Success.js
@@ -50,11 +50,11 @@ const Success = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="0.6"
+                strokeWidth="0.6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
--- components/CompletePayment/UsingPin.js
+++ components/CompletePayment/UsingPin.js
@@ -88,11 +88,11 @@ const UsingPin = ({
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="1"
+                  strokeWidth="1"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
