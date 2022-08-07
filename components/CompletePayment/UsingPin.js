df408dfa53e1c9ea7f8cc829e4e61b1e4e5395ef
“Akpughe” <davidakpughe2@gmail.com>
2022-08-07 19:42:34 +0200

    add - form navigation

---
+++ components/CompletePayment/UsingPin.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+const UsingPin = ({ price, handleShow }) => {
+  return (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+      <div className="fixed inset-0 z-10 grid place-items-center overflow-y-auto">
+        <div className="flex min-h-full w-1/4 items-end justify-center text-center sm:items-center sm:p-0">
+          <form className="relative w-full transform space-y-12 overflow-hidden rounded-lg bg-white p-10 text-left shadow-xl transition-all sm:my-8">
+            <div className="bg-white">
+              <div className="">
+                <div className="space-y-10 text-center sm:mt-0 sm:text-left">
+                  <div className="flex justify-between">
+                    <div className="space-y-2">
+                      <h3
+                        className="text-4xl font-medium leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        ₦ {price}
+                      </h3>
+                    </div>
+                    <svg
+                      onClick={() => handleShow('')}
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 cursor-pointer hover:opacity-70"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
+
+                  <div className="text-lg capitalize font-semibold">
+                    Complete your payment
+                  </div>
+
+                  <ul className="grid w-full gap-6">
+                    <input
+                      type="text"
+                      className="mt-0 w-full border-0 border-b-2 border-gray-200 px-0.5 focus:border-black focus:ring-0"
+                      placeholder="Your eNaira Email"
+                      required
+                    />
+                    <input
+                      type="text"
+                      className="mt-0 w-full border-0 border-b-2 border-gray-200 px-0.5 focus:border-black focus:ring-0"
+                      placeholder="Your eNaira Password"
+                      required
+                    />
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <button
+                type="submit"
+                className="bg-dune-brown hover:bg-dune-brown btn btn-block flex items-center justify-between rounded-md border-none px-6 capitalize"
+              >
+                <div>Complete Payment</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="1"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <div className="flex items-center space-x-1 text-xs opacity-60 grayscale">
+              <div
+                className="h-4s w-4 rounded-full border bg-cover bg-center p-2"
+                style={{
+                  backgroundImage:
+                    "url('https://21stcenturychronicle.com/wp-content/uploads/2021/11/e-naira-logo.png')",
+                }}
+              ></div>
+              <div>Powered by eNaira</div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UsingPin;
--- pages/pay/[pay_id].js
+++ pages/pay/[pay_id].js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { AiOutlineClose } from 'react-icons/ai';
+import UsingEmail from '../../components/CompletePayment/UsingEmail';
+import UsingPin from '../../components/CompletePayment/UsingPin';
 
 const customStyles = {
   // top: '50%',
@@ -22,6 +24,18 @@ const PayId = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [useEmail, setUseEmail] = useState(false);
+  const [usePin, setUsePin] = useState(false);
+  const [price, setPrice] = useState();
+  const [show, setShow] = useState('');
+
+  const moveToPayment = () => {
+    useEmail ? setShow('Email') : usePin ? setShow('Pin') : '';
+  };
+
+  const handleShow = (str) => {
+    setShow(str);
+  };
 
   useEffect(() => {
     getPaymentData();
@@ -38,88 +52,144 @@ const PayId = () => {
       }
     );
     console.log('response', res.data);
+    setPrice(res.data?.data?.amount);
   };
 
-  return (
-    <div className=' h-screen flex justify-center '>
-      <div style={customStyles}>
-        {' '}
-        <div className="flex justify-end pr-4 pt-2">
-          <button onClick={''}>
-            <AiOutlineClose size={25} />
-          </button>
-        </div>
-        <div className="py-8 px-10">
-          <p className="text-5xl">N 120000</p>
-        </div>
-        <div
-          className="flex flex-col justify-center"
-          style={{ height: 'calc(100% - 100px)' }}
-        >
-          <form onSubmit={(e) => createPaymentLink(e)} className="px-10">
-            <label className="block">
-              <span className="text-black text-xs uppercase font-light tracking-[0.34em]">
-                Email
-              </span>
-              <input
-                type="email"
-                value={email}
-                className="
-            mt-6
-            block
-            w-full
-            px-0.5
-            border-0 border-b border-black
-            focus:ring-0 focus:border-black
-          "
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </label>
-            <label className="block mt-12">
-              <span className="text-black text-xs uppercase font-light tracking-[0.34em]">
-                Password
-              </span>
-              <input
-                type="password"
-                value={password}
-                className="
-            mt-6
-            block
-            w-full
-            px-0.5
-            border-0 border-b border-black
-            focus:ring-0 focus:border-black
-          "
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </label>
-            <div className="flex justify-between w-full my-8">
+  console.log('price', price);
+
+  return show === '' ? (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+      <div className="fixed inset-0 z-10 grid place-items-center overflow-y-auto">
+        <div className="flex min-h-full w-1/4 items-end justify-center text-center sm:items-center sm:p-0">
+          <form
+            onSubmit={moveToPayment}
+            className="relative w-full transform space-y-12 overflow-hidden rounded-lg bg-white p-10 text-left shadow-xl transition-all sm:my-8"
+          >
+            <div className="bg-white">
+              <div className="">
+                <div className="space-y-10 text-center sm:mt-0 sm:text-left">
+                  <div className="flex justify-between">
+                    <div className="space-y-2">
+                      <h3
+                        className="text-4xl font-medium leading-6 text-gray-900"
+                        id="modal-title"
+                      >
+                        ₦ {price}
+                      </h3>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 cursor-pointer hover:opacity-70"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </div>
+
+                  <div className="text-lg">
+                    Select a way to pay from your eNaira wallet.
+                  </div>
+
+                  <ul className="grid w-full gap-6">
+                    <li onClick={() => setUseEmail(true)}>
+                      <input
+                        type="radio"
+                        id="hosting-small"
+                        name="hosting"
+                        value="hosting-small"
+                        className="peer hidden"
+                        required
+                      />
+                      <label
+                        for="hosting-small"
+                        className="gray-300 d:text-blue-500 peer-checked:border-dune-brown peer-checked:text-dune-brown inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600"
+                      >
+                        <div className="block">
+                          <div className="w-full font-semibold">
+                            Email and password
+                          </div>
+                        </div>
+                      </label>
+                    </li>
+                    <li onClick={() => setUsePin(true)}>
+                      <input
+                        type="radio"
+                        id="hosting-big"
+                        name="hosting"
+                        value="hosting-big"
+                        className="peer hidden"
+                      />
+                      <label
+                        for="hosting-big"
+                        className="gray-300 peer-checked:border-dune-brown peer-checked:text-dune-brown inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600"
+                      >
+                        <div className="block">
+                          <div className="w-full font-semibold">
+                            Phone number &amp; PIN
+                          </div>
+                        </div>
+                      </label>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="">
               <button
                 type="submit"
-                className="uppercase font-semibold text-base tracking-[0.34em]"
+                className="bg-dune-brown hover:bg-dune-brown btn btn-block flex items-center justify-between rounded-md border-none px-6 capitalize"
               >
-                authorize & pay
+                <div>Continue</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="1"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  />
+                </svg>
               </button>
-              <svg
-                width="87"
-                height="24"
-                viewBox="0 0 87 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M86.0607 13.0607C86.6464 12.4749 86.6464 11.5251 86.0607 10.9393L76.5147 1.3934C75.9289 0.807611 74.9792 0.807611 74.3934 1.3934C73.8076 1.97919 73.8076 2.92893 74.3934 3.51472L82.8787 12L74.3934 20.4853C73.8076 21.0711 73.8076 22.0208 74.3934 22.6066C74.9792 23.1924 75.9289 23.1924 76.5147 22.6066L86.0607 13.0607ZM0 13.5H85V10.5H0V13.5Z"
-                  fill="black"
-                />
-              </svg>
+            </div>
+
+            <div className="flex items-center space-x-1 text-xs opacity-60 grayscale">
+              <div
+                className="p-2 rounded-full border bg-center bg-cover w-4 h-4s"
+                style={{
+                  backgroundImage:
+                    "url('https://21stcenturychronicle.com/wp-content/uploads/2021/11/e-naira-logo.png')",
+                }}
+              ></div>
+              <div> Powered by eNaira</div>
             </div>
           </form>
         </div>
       </div>
     </div>
-  );
+  ) : show === 'Email' ? (
+    <UsingEmail price={price} handleShow={handleShow} />
+  ) : show === 'Pin' ? (
+    <UsingPin price={price} handleShow={handleShow} />
+  ) : null;
 };
 
 export default PayId;
