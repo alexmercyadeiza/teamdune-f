565de6b641a7341a7d896bf9922483a0c8fbb1bf
“Akpughe” <davidakpughe2@gmail.com>
2022-08-09 22:22:42 +0200

    fix - loader color

--- components/CompletePayment/UsingPin.js
+++ components/CompletePayment/UsingPin.js
@@ -27,7 +27,7 @@ const UsingPin = ({
                 size={40}
                 lineWeight={5}
                 speed={2}
-                color="text-dune-brown"
+                color="black"
               />
             </div>
           )}
